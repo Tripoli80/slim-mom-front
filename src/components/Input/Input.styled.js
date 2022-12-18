f30df2cba2d40f7ed65891d6745bc1904172debd
Yurii Kucher <dev@kucha.kiev.ua>
2022-12-18 22:08:00 +0200

    add radiobuttons styles

--- src/components/Input/Input.styled.js
+++ src/components/Input/Input.styled.js
@@ -18,18 +18,64 @@ export const InputValue = styled.input.attrs(props => ({
   outline: none;
   width: 100%;
 `;
-
 export const Label = styled.label`
   display: flex;
   color: #9b9faa;
 `;
-
 export const LabelRadio = styled.label`
   display: inline-flex;
+  cursor: pointer;
 `;
 export const InputRadio = styled.input`
   margin-right: 8px;
+  /* hidden */
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  /* -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none; */
 `;
+
+export const InputRadioStyled = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 8px;
+  width: 20px;
+  height: 20px;
+  background: #ffffff;
+  border: 1px solid #e0e0e0;
+  border-radius: 50%;
+  ${InputRadio}:checked + && {
+    ::before {
+      content: '';
+      display: block;
+      width: 10px;
+      height: 10px;
+      background-color: #fc842d;
+      border-radius: 50%;
+    }
+  }
+`;
+
+export const LabelRadioText = styled.span`
+  display: inline-flex;
+  font-family: 'Verdana';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 17px;
+  letter-spacing: 0.04em;
+  color: #9b9faa;
+  ${InputRadio}:checked ~ && {
+    font-weight: 700;
+    color: #fc842d;
+  }
+`;
+
 export const WrapperRadio = styled.div`
   display: flex;
   justify-content: space-between;
