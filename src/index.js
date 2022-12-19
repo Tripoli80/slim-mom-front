498f350f57e6341d3ba03112829aa8580e1ef302
Tripoli80 <tripoli80@gmail.com>
2022-12-19 10:31:41 +0100

    redux commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './components/App/App';
-<<<<<<< HEAD
-=======
-import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
->>>>>>> reduxjs
+import App from './components/App/App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
