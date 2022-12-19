97747d2c5f550d8c797a1ef0b46200fcb5c0ceea
IvanChudiyovuch <ivanchudiyovuch@ukr.net>
2022-12-19 20:50:56 +0200

    head4
    Додав стилів

--- src/components/Header/AppBar.js
+++ src/components/Header/AppBar.js
@@ -3,14 +3,39 @@ import { AuthNav } from '../AuthNav/AuthNav';
 import { UserInfo } from '../UserInfo/UserInfo';
 import { Header } from './AppBar.styled';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { useIsLoading } from '../../hooks/hooks';
+import { Logo } from '../Logo/Logo';
+import { Link, BurgerMenu } from './AppBar.styled';
 
 export const AppBar = () => {
+  const { isLoggedIn } = useIsLoading();
   return (
     <Header>
-      <Navigation />
+      <Link to="/">
+        <Logo />
+      </Link>
+      {isLoggedIn && (
+        <>
+          <Navigation />
+          <UserInfo />
+          <BurgerMenu>
+            <GiHamburgerMenu />
+          </BurgerMenu>
+        </>
+      )}
       <AuthNav />
-      <UserInfo />
-      <GiHamburgerMenu />
     </Header>
+
+    // <Header>
+    //   <Link to="/">
+    //     <Logo />
+    //   </Link>
+    //   <Navigation />
+    //   <UserInfo />
+    //   <BurgerMenu>
+    //     <GiHamburgerMenu />
+    //   </BurgerMenu>
+    //   {isLoggedIn && <AuthNav />}
+    // </Header>
   );
 };
--- src/components/Logo/Logo.styled.js
+++ src/components/Logo/Logo.styled.js
@@ -4,13 +4,18 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: flex-end;
   margin: 0;
+  flex-grow: 1;
+
+  @media (min-width: 1280px) {
+    margin-right: 20px;
+  }
 `;
 
 export const Img = styled.img`
   width: 44px;
   height: 47px;
 
-  @media screen and (min-width: 1200px) {
+  @media (min-width: 1280px) {
     width: 66px;
     height: 71px;
   }
@@ -19,20 +24,38 @@ export const Img = styled.img`
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  margin-left: -15px;
-  padding-right: 20px;
 
   font-size: 18px;
+
+  @media (min-width: 768px) {
+    margin-left: 10px;
+    margin-bottom: 20px;
+  }
+
+  @media (min-width: 1280px) {
+    margin-left: -15px;
+    margin-bottom: 0;
+  }
 `;
 
 export const TextMom = styled.p`
-  text-transform: capitalize;
-  color: #fc842d;
-  margin: 0;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+    text-transform: capitalize;
+    color: #fc842d;
+    margin: 0;
+  }
 `;
 
 export const TextSlim = styled.p`
-  text-transform: capitalize;
-  color: #212121;
-  margin: 0 6px 0 0;
+  display: none;
+
+  @media (min-width: 768px) {
+    display: block;
+    text-transform: capitalize;
+    color: #212121;
+    margin: 0 6px 0 0;
+  }
 `;
