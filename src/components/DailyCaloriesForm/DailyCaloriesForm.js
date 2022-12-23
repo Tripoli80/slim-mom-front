260b810351e5ed15252354b37c803a9855f494e5
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-12-23 10:10:50 +0200

    Update DailyCaloriesForm.js
    фікс від qa

--- src/components/DailyCaloriesForm/DailyCaloriesForm.js
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.js
@@ -38,22 +38,22 @@ const DailyCaloriesForm = () => {
   const validation = Yup.object().shape({
     height: Yup.number()
       .typeError('Must be a number')
-      .min(100, 'Minimal 120')
-      .max(260, 'Max 260')
+      .min(100, 'Minimal 20')
+      .max(250, 'Max 250')
       .required('Required!'),
     age: Yup.number()
       .typeError('Must be a number')
       .min(18, 'Minimal 18')
-      .max(120, 'Max 120')
+      .max(100, 'Max 100')
       .required('Required!'),
     cWeight: Yup.number()
       .typeError('Must be a number')
-      .min(50, 'Minimal 50')
-      .max(250, 'Max 250')
+      .min(20, 'Minimal 20')
+      .max(500, 'Max 500')
       .required('Required!'),
     dWeight: Yup.number()
       .typeError('Must be a number')
-      .min(45, 'Minimal 45')
+      .min(20, 'Minimal 20')
       .max(
         Yup.ref('cWeight'),
         'The maximum value cannot be greater than the current one.'
@@ -102,114 +102,110 @@ const DailyCaloriesForm = () => {
   const { handleSubmit, handleChange, values, touched, errors } = formik;
   return (
     <>
-        <Container>
-          <Form onSubmit={handleSubmit}>
-            <Title>{Translator('calculateYourDaily')}</Title>
-            <Wrapper>
-              <Section>
-                <Input
-                  placeHolder={Translator('height')}
-                  id="height"
-                  name="height"
-                  onChange={handleChange}
-                  value={values.height}
-                />
-                {touched.height && <ErrorMessage>{errors.height}</ErrorMessage>}
+      <Container>
+        <Form onSubmit={handleSubmit}>
+          <Title>{Translator('calculateYourDaily')}</Title>
+          <Wrapper>
+            <Section>
+              <Input
+                placeHolder={Translator('height')}
+                id="height"
+                name="height"
+                onChange={handleChange}
+                value={values.height}
+              />
+              {touched.height && <ErrorMessage>{errors.height}</ErrorMessage>}
 
-                <Input
-                  placeHolder={Translator('age')}
-                  id="age"
-                  name="age"
-                  type="text"
-                  onChange={handleChange}
-                  value={values.age}
-                />
-                {touched.age && <ErrorMessage>{errors.age}</ErrorMessage>}
+              <Input
+                placeHolder={Translator('age')}
+                id="age"
+                name="age"
+                type="text"
+                onChange={handleChange}
+                value={values.age}
+              />
+              {touched.age && <ErrorMessage>{errors.age}</ErrorMessage>}
 
-                <Input
-                  placeHolder={Translator('currentWeight')}
-                  id="cWeight"
-                  name="cWeight"
-                  type="text"
-                  onChange={handleChange}
-                  value={values.cWeight}
-                />
-                {touched.cWeight && (
-                  <ErrorMessage>{errors.cWeight}</ErrorMessage>
-                )}
-              </Section>
-              <div>
-                <Input
-                  placeHolder={Translator('desiredWeight')}
-                  id="dWeight"
-                  name="dWeight"
-                  type="text"
-                  onChange={handleChange}
-                  value={values.dWeight}
-                />
-                {touched.dWeight && (
-                  <ErrorMessage>{errors.dWeight}</ErrorMessage>
-                )}
+              <Input
+                placeHolder={Translator('currentWeight')}
+                id="cWeight"
+                name="cWeight"
+                type="text"
+                onChange={handleChange}
+                value={values.cWeight}
+              />
+              {touched.cWeight && <ErrorMessage>{errors.cWeight}</ErrorMessage>}
+            </Section>
+            <div>
+              <Input
+                placeHolder={Translator('desiredWeight')}
+                id="dWeight"
+                name="dWeight"
+                type="text"
+                onChange={handleChange}
+                value={values.dWeight}
+              />
+              {touched.dWeight && <ErrorMessage>{errors.dWeight}</ErrorMessage>}
 
-                <Box>
-                  <TitleRaioGroup>{Translator('bloodType')}</TitleRaioGroup>
-                  <WrapperRadio>
-                    <LabelRadio>
-                      <InputRadio
-                        name="blood"
-                        type="radio"
-                        value="1"
-                        onChange={handleChange}
-                        checked={values.blood === '1'}
-                      />
-                      <InputRadioStyled />
-                      <LabelRadioText>1</LabelRadioText>
-                    </LabelRadio>
-                    <LabelRadio>
-                      <InputRadio
-                        name="blood"
-                        type="radio"
-                        onChange={handleChange}
-                        value="2"
-                        checked={values.blood === '2'}
-                      />
-                      <InputRadioStyled />
-                      <LabelRadioText>2</LabelRadioText>
-                    </LabelRadio>
-                    <LabelRadio>
-                      <InputRadio
-                        name="blood"
-                        type="radio"
-                        onChange={handleChange}
-                        value="3"
-                        checked={values.blood === '3'}
-                      />
-                      <InputRadioStyled />
-                      <LabelRadioText>3</LabelRadioText>
-                    </LabelRadio>
-                    <LabelRadio>
-                      <InputRadio
-                        name="blood"
-                        type="radio"
-                        onChange={handleChange}
-                        value="4"
-                        checked={values.blood === '4'}
-                      />
-                      <InputRadioStyled />
-                      <LabelRadioText>4</LabelRadioText>
-                    </LabelRadio>
-                  </WrapperRadio>
-                </Box>
-                {touched.blood && <ErrorMessage>{errors.blood}</ErrorMessage>}
-              </div>
-            </Wrapper>
-            <BtnSub>
-              <Button type="submit" onSubmit={handleSubmit}>
-                {Translator('submit')}
-              </Button>
-            </BtnSub>
-          </Form>
-        </Container>
+              <Box>
+                <TitleRaioGroup>{Translator('bloodType')}</TitleRaioGroup>
+                <WrapperRadio>
+                  <LabelRadio>
+                    <InputRadio
+                      name="blood"
+                      type="radio"
+                      value="1"
+                      onChange={handleChange}
+                      checked={values.blood === '1' || true}
+                    />
+                    <InputRadioStyled />
+                    <LabelRadioText>1</LabelRadioText>
+                  </LabelRadio>
+                  <LabelRadio>
+                    <InputRadio
+                      name="blood"
+                      type="radio"
+                      onChange={handleChange}
+                      value="2"
+                      checked={values.blood === '2'}
+                    />
+                    <InputRadioStyled />
+                    <LabelRadioText>2</LabelRadioText>
+                  </LabelRadio>
+                  <LabelRadio>
+                    <InputRadio
+                      name="blood"
+                      type="radio"
+                      onChange={handleChange}
+                      value="3"
+                      checked={values.blood === '3'}
+                    />
+                    <InputRadioStyled />
+                    <LabelRadioText>3</LabelRadioText>
+                  </LabelRadio>
+                  <LabelRadio>
+                    <InputRadio
+                      name="blood"
+                      type="radio"
+                      onChange={handleChange}
+                      value="4"
+                      checked={values.blood === '4'}
+                    />
+                    <InputRadioStyled />
+                    <LabelRadioText>4</LabelRadioText>
+                  </LabelRadio>
+                </WrapperRadio>
+              </Box>
+              {touched.blood && <ErrorMessage>{errors.blood}</ErrorMessage>}
+            </div>
+          </Wrapper>
+          <BtnSub>
+            <Button type="submit" onSubmit={handleSubmit}>
+              {Translator('Start losing weight')}
+            </Button>
+          </BtnSub>
+        </Form>
+      </Container>
       {isOpen && (
         <Modal
           onClose={() => {
