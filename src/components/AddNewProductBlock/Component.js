fa2e5dabf330367e9f422187c94475feadf60d0a
Yurii Kucher <dev@kucha.kiev.ua>
2022-12-25 01:52:50 +0200

    add margins

--- src/components/AddNewProductBlock/Component.js
+++ src/components/AddNewProductBlock/Component.js
@@ -1,7 +1,15 @@
+import Container from 'components/Container/Container';
+import { Wrapper } from './Component.styled';
 import AddNewProductForm from 'components/AddNewProductForm/Component';
 
 const AddNewProductBlock = () => {
-  return <AddNewProductForm />;
+  return (
+    <Wrapper>
+      <Container>
+        <AddNewProductForm />
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default AddNewProductBlock;
