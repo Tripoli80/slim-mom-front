c78a5124f8b8f374d7e1639fc310bd5f482ed778
Yurii Kucher <dev@kucha.kiev.ua>
2022-12-24 23:03:59 +0200

    add formik

--- src/components/AddNewProductForm/Component.js
+++ src/components/AddNewProductForm/Component.js
@@ -1,33 +1,96 @@
-const AddNewProductForm = () => {
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
+import { Formik } from 'formik';
 
-    const formdata = {
-      title: form.elements.title.value,
-      categories: form.elements.categories.value,
-      calories: form.elements.calories.value,
-    };
-    console.log('formdata:', formdata);
-    form.reset();
-  };
+const AddNewProductForm = () => {
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   const formdata = {
+  //     title: form.elements.title.value,
+  //     categories: form.elements.categories.value,
+  //     calories: form.elements.calories.value,
+  //   };
+  //   console.log('formdata:', formdata);
+  //   form.reset();
+  // };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label>
-        title
-        <input type="text" name="title" />
-      </label>
-      <label>
-        categories
-        <input type="text" name="categories" />
-      </label>
-      <label>
-        calories
-        <input type="number" name="calories" />
-      </label>
-      <button type="submit">Add product</button>
-    </form>
+    <Formik
+      initialValues={{ title: '', categories: '', calories: '' }}
+      onSubmit={values => {
+        // const form = e.currentTarget;
+        // const formdata = {
+        //   title: form.elements.title.value,
+        //   categories: form.elements.categories.value,
+        //   calories: form.elements.calories.value,
+        // };
+        console.log('values:', values);
+        // form.reset();
+      }}
+      validate={values => {
+        const errors = {};
+        if (!values.title) {
+          errors.title = 'Required';
+        } else if (!values.categories) {
+          errors.categories = 'Required';
+        } else if (!values.calories) {
+          errors.calories = 'Required';
+        }
+        return errors;
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <label>
+            title
+            <input
+              type="text"
+              name="title"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.titles}
+            />
+            {errors.title && touched.title && errors.title}
+          </label>
+          <label>
+            categories
+            <input
+              type="text"
+              name="categories"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.categories}
+            />
+            {errors.categories && touched.categories && errors.categories}
+          </label>
+          <label>
+            calories
+            <input
+              type="number"
+              name="calories"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.calories}
+            />
+            {errors.calories && touched.calories && errors.calories}
+          </label>
+          <button
+            type="submit"
+            // disabled={!values.titles || !values.categories || !values.calories}
+          >
+            Add product
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
