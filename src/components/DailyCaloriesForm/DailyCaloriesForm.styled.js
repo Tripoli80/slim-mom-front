539d7d6e883481dedd2c9f2f9760f1bd33d7f18f
Mark <markfamiliytitan@gmail.com>
2022-12-26 00:03:08 +0200

    почистив імпорти + поправив форму

--- src/components/DailyCaloriesForm/DailyCaloriesForm.styled.js
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.styled.js
@@ -1,9 +1,4 @@
 import styled from 'styled-components';
-import Banan from './img/Layer-9.png';
-import Vector from './img/Vector3.png';
-import Lust from './img/Layer-41.png';
-import Strawberry from './img/Strawberry-Big-PNG.png';
-import FrameDeck from './img/Frame1.png';
 export const Form = styled.form`
   display: flex;
   flex-wrap: wrap;
@@ -60,32 +55,6 @@ export const Section = styled.div`
   }
 `;
 
-export const Image = styled.div`
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  
-  @media (min-width: 768px) {
-    // background-image: url(${Lust}), 
-    // url(${Strawberry}), 
-    // url(${Banan}),
-      // url(${Vector});
-    // background-repeat: no-repeat;
-    // background-size: 531px 602px, 300px 300px, 740px 527px, 803px 750px;
-    // background-position: left 0 bottom 0, right 200px bottom 200px,
-    //   right 0 bottom 0, right 0 bottom 0;
-  }
-  @media (min-width: 1264px) {
-    // background-image: url(${FrameDeck});
-    // background-size: 980px 820px;
-    // background-position: right 0 top 0;
-  }
-  @media (min-width: 1700px) {
-    // background-image: url(${FrameDeck}), url(${Strawberry});
-    // background-size: 531px 602px, 300px 300px;
-    // background-position: left 0 bottom 0, right 200px bottom 200px;
-  }
-`;
 export const ErrorMessage = styled.p`
   position: absolute;
   color: rgb(200, 10, 65);
