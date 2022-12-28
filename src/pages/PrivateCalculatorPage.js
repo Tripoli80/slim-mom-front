4977d79d09c238362949ee8145a87c9682f418e2
AntoninaTarasyuk <antoshka.tarasyuk@gmail.com>
2022-12-29 01:22:49 +0200

    Update PrivateCalculatorPage.js

--- src/pages/PrivateCalculatorPage.js
+++ src/pages/PrivateCalculatorPage.js
@@ -6,10 +6,12 @@ import Sidebar from 'components/SideBar/Sidebar';
 const CalcPageWrap = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 40px;
   @media screen and (min-width: 1280px) {
     width: 714px;
     flex-direction: row;
     justify-content: space-between;
+    gap: 0;
   }
 `
 export const PrivateCalculatorPage = () => {
